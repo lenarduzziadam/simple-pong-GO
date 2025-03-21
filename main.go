371ee9189d1f679ec22f8d41f4c892f4a567e5bf
lenarduzziadam <lenarduzziadam@gmail.com>
2025-03-21 15:27:13 -0500

    want to turn this into breakout eventually

--- main.go
+++ main.go
@@ -13,10 +13,10 @@ import (
 )
 
 const (
-	screenWidth  = 650
-	screenHeight = 450
-	ballSpeed    = 3
-	paddleSpeed  = 6
+	screenWidth  = 1550
+	screenHeight = 1000
+	ballSpeed    = 5
+	paddleSpeed  = 8
 )
 
 type Object struct {
@@ -43,7 +43,30 @@ type Game struct {
 func main() {
 	ebiten.SetWindowTitle("Pong in Ebitengine")
 	ebiten.SetWindowSize(screenWidth, screenHeight)
-	g := &Game{}
+
+	paddle := Paddle{
+		Object: Object{
+			X: 1500,
+			Y: 750,
+			W: 15,
+			H: 90,
+		},
+	}
+	ball := Ball{
+		Object: Object{
+			X: 0,
+			Y: 0,
+			W: 15,
+			H: 15,
+		},
+		dxdt: ballSpeed,
+		dydt: ballSpeed,
+	}
+
+	g := &Game{
+		paddle: paddle,
+		ball:   ball,
+	}
 	err := ebiten.RunGame(g)
 	if err != nil {
 		log.Fatal(err)
@@ -67,8 +90,62 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	)
 	scoreStr := "Score: " + fmt.Sprint(g.score)
 	text.Draw(screen, scoreStr, basicfont.Face7x13, 10, 10, color.White)
+
+	highScoreStr := "High Score: " + fmt.Sprint(g.highScore)
+	text.Draw(screen, highScoreStr, basicfont.Face7x13, 10, 30, color.White)
 }
 
 func (g *Game) Update() error {
+	g.paddle.MoveOnKeyPress()
+	g.ball.Move()
+	g.CollideWithWall()
+	g.CollideWithPaddle()
 	return nil
+}
+
+func (p *Paddle) MoveOnKeyPress() {
+	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
+		p.Y += paddleSpeed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
+		p.Y -= paddleSpeed
+	}
+
+}
+
+func (b *Ball) Move() {
+	b.X += b.dxdt
+	b.Y += b.dydt
+}
+
+func (g *Game) Reset() {
+	g.ball.X = 0
+	g.ball.Y = 0
+
+	g.score = 0
+}
+
+func (g *Game) CollideWithWall() {
+	if g.ball.X >= screenWidth {
+		g.Reset()
+
+	} else if g.ball.X <= 0 {
+		g.ball.dxdt = ballSpeed
+
+	} else if g.ball.Y <= 0 {
+		g.ball.dydt = ballSpeed
+
+	} else if g.ball.Y >= screenHeight {
+		g.ball.dydt = -ballSpeed
+	}
+}
+
+func (g *Game) CollideWithPaddle() {
+	if g.ball.X >= g.paddle.X && g.ball.Y >= g.paddle.Y && g.ball.Y <= g.paddle.Y+g.paddle.H {
+		g.ball.dxdt = -g.ball.dxdt
+		g.score++
+		if g.score > g.highScore {
+			g.highScore = g.score
+		}
+	}
 }
